c9b2d21b15db58594a8a57a55227b3381898f060
Lucas Pasqualin <lpasqualin@meta.com>
2025-08-29 08:58:42 -0700

    forgot await

--- torchstore/_state_dict_utils.py
+++ torchstore/_state_dict_utils.py
@@ -23,12 +23,9 @@ async def push_state_dict(store, state_dict, key):
 
     """
     flattened_state_dict, mapping = flatten_state_dict(state_dict)
-    puts = []
     for flattened_key, value in flattened_state_dict.items():
-        print(f"Putting {flattened_key} with {value=}")
-        store.put(f"{key}{DELIM}{flattened_key}", value)
-        # puts.append(store.put(f"{key}{DELIM}{flattened_key}", value))
-    asyncio.gather(*puts)
+        print(f"Putting {flattened_key} with {value.shape=} {value.dtype=}")
+        await store.put(f"{key}{DELIM}{flattened_key}", value)
 
     await store.put(f"{key}{DELIM}{MAPPING}", mapping)
 
