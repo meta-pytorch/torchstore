3a9b79e24c9a88079d56b2cdde310127e5db772f
Kai Li <lky@meta.com>
2025-09-09 07:45:10 -0700

    ufmt api.py

--- torchstore/api.py
+++ torchstore/api.py
@@ -7,9 +7,9 @@
 from typing import Any, Dict, Optional, Union
 
 import torch
+from monarch.actor import get_or_spawn_controller
 
 import torchstore.state_dict_utils
-from monarch.actor import get_or_spawn_controller
 from torchstore.client import LocalClient
 from torchstore.controller import Controller
 from torchstore.storage_volume import StorageVolume
