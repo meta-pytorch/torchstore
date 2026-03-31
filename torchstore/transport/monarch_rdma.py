ccce9b522b0c9bb859d9abbe81eaa2fab2477656
Danielle Pintz <daniellepintz@gmail.com>
2026-03-31 06:39:47 -0700

    Fix old monarch RDMA check to use is_rdma_available
    The pre-0.4.0 monarch API is is_rdma_available, not is_rdma_enabled.

--- torchstore/transport/monarch_rdma.py
+++ torchstore/transport/monarch_rdma.py
@@ -18,7 +18,7 @@ try:
         from monarch.rdma import is_ibverbs_available as monarch_rdma_available
     except ImportError:
         # monarch < 0.4.0
-        from monarch.rdma import is_rdma_enabled as monarch_rdma_available
+        from monarch.rdma import is_rdma_available as monarch_rdma_available
 except ImportError:
     monarch_rdma_available = lambda: False
 
